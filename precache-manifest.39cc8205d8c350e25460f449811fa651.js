5632d08a00c1cc04cf926a7f55bea9aef6d8cdd7
Sodiq <sodiq.akanmu001@gmail.com>
2020-09-22 02:50:18 +0100

    Updates

---
+++ precache-manifest.39cc8205d8c350e25460f449811fa651.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10ca3a2e6014c0631c5fb2ab7f3a1a43",
+    "url": "/landing-page/index.html"
+  },
+  {
+    "revision": "811d1b06e41b41f65708",
+    "url": "/landing-page/static/css/main.28ea4110.chunk.css"
+  },
+  {
+    "revision": "03f1422b7232bc5e3cc7",
+    "url": "/landing-page/static/js/2.e22f8150.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/landing-page/static/js/2.e22f8150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "811d1b06e41b41f65708",
+    "url": "/landing-page/static/js/main.0401611b.chunk.js"
+  },
+  {
+    "revision": "d803e7ff0721cd373432",
+    "url": "/landing-page/static/js/runtime-main.8c4e35fe.js"
+  },
+  {
+    "revision": "1b483f5f3e73468f0b48a40dcf400b13",
+    "url": "/landing-page/static/media/Group 3.1b483f5f.svg"
+  },
+  {
+    "revision": "5c9d6a1ba1accf18cf8c35979163c0e3",
+    "url": "/landing-page/static/media/Vezeti.5c9d6a1b.png"
+  },
+  {
+    "revision": "ef49522bc40b1a053d72540dcef357dd",
+    "url": "/landing-page/static/media/about.ef49522b.jpg"
+  },
+  {
+    "revision": "d2063cac0eceb31c246ca46bd02cde73",
+    "url": "/landing-page/static/media/businesswoman2.d2063cac.svg"
+  },
+  {
+    "revision": "c0c4002332122706fe3af2bd4cc69167",
+    "url": "/landing-page/static/media/female-programmer.c0c40023.webp"
+  },
+  {
+    "revision": "7edfb136a6dc6e38989674fb12644fdb",
+    "url": "/landing-page/static/media/man.7edfb136.jpg"
+  }
+]);
